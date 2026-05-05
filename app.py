720226a8bbea9af55e064fda0221369277108a49
iliasathan35-gr <iliasathan35@gmail.com>
2026-05-05 19:58:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def logout():
 # --------------------
 @app.route("/success")
 def success():
-    return "OK 💈"
+    return "✅ Επιτυχία! Το ραντεβού σου καταχωρήθηκε."
 
 # --------------------
 # RUN
